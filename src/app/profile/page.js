f60a8f7f0b1947e86c0bb0739a592125370b6e3a
BismaWahyu <bw.anaafie@gmail.com>
2024-01-15 15:58:56 +0700

    profile page placeholder

---
+++ src/app/profile/page.js
@@ -0,0 +1,10 @@
+import Navbar from "@/components/Navbar";
+
+export default function Profile(){
+    return (
+        <div className="min-h-screen bg-slate-200">
+            <Navbar />
+            <h1>Profile will be here</h1>
+        </div>
+    )
+}
