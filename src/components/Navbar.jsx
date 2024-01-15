3ef1ab63e2d64067fe2e2d0aeeaae12bdf34cb8d
BismaWahyu <bw.anaafie@gmail.com>
2024-01-15 22:40:06 +0700

    fixing navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,31 +2,56 @@ import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 import { FaUnlockKeyhole } from "react-icons/fa6";
+import Search from "../../public/svgs/search";
+import HeartOutline from "../../public/svgs/heart-outline";
+import RedBall from "../../public/svgs/redBall";
+import Bell from "../../public/svgs/bell";
+import Map from "../../public/svgs/map";
 
 
 const Navbar = () => {
   return (
-    <nav className="flex bg-white w-full">
-      <div className="flex container py-4 pl-16  items-center justify-between ">
-        <div className="ml-3">
-          <Link href="/">
-            <Image src="/images/logo.png" alt="" width={158} height={28} />
+    <header className="lg:flex bg-white w-screen">
+      <div className="px-[20px] lg:px-0 flex lg:w-[1280px] mx-auto lg:h-[81px] h-[56px] items-center justify-between">
+        <div className="">
+          <Link href="/" className="grid items-center">
+            <Image
+              className="w-[144.2px] h-[28px]"
+              src="/images/logo.png"
+              alt=""
+              width={500}
+              height={500}
+            />
           </Link>
         </div>
-        <form action="">
-          <div className="flex gap-2">
-
-            <div className="form-group">
-              <input className="w-[180px] bg-[#F4F5F7] px-4 py-2 rounded-md text-sm" type="text" placeholder="Indonesia" />
-
+        <section className="hidden lg:contents">
+          <form>
+            <div className="flex gap-[2px]">
+              <div className="form-group relative">
+                <input
+                  className="w-[189px] h-[44px] bg-[#F4F5F7] pl-12 rounded-l-[8px] text-sm"
+                  type="text"
+                  placeholder="Indonesia"
+                />
+                <button className="flex absolute left-4 top-0 mr-3 mt-3 text-gray-400">
+                  <Map />
+                </button>
+              </div>
+              <div className="form-group">
+                <input
+                  className="w-[389px] h-[44px] bg-[#F4F5F7] px-4 py-2 text-sm"
+                  type="text"
+                  placeholder="Cari yang kamu mau di sini..."
+                />
+              </div>
+              <botton className="cursor-pointer w-[52px] bg-[#F4F5F7] flex justify-center items-center rounded-r-[8px]">
+                <button>
+                  <Search />
+                </button>
+              </botton>
             </div>
-            <div className="form-group">
-              <input className="w-[390px] bg-[#F4F5F7] px-4 py-2 rounded-md text-sm" type="text" placeholder="Cari yang kamu mau di sini..." />
-
-            </div>
-          </div>
-
-        </form>
+          </form>
+        </section>
 
         <div className=" ">
           <button
@@ -40,7 +65,7 @@ const Navbar = () => {
           </button>
         </div>
       </div>
-    </nav>
+    </header>
   );
 };
 
